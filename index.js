b174acbbbd423227849b5dddfec3e898ce1e9749
Abhishek <abhi2001panchal@gmai.com>
2026-05-29 17:14:04 +0000

    Learned prettier setup, importance of auto-reload, and dotenv

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env"
+})
+
+let myUserName = process.env.user_name;
+console.log(`user name: ${myUserName}`)
+
 console.log("Start of project management backend project...");
+console.log("auto restart works perfectly fine...");
