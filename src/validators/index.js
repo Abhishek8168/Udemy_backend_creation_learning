fe4a054c2e02e2eab3862ebc910b04634cd31a3d
Abhishek <abhi2001panchal@gmai.com>
2026-06-11 07:28:13 +0000

    Finished user work

--- src/validators/index.js
+++ src/validators/index.js
@@ -9,6 +9,35 @@ const userRegisterValidator = () => {
     ]
 }
 
+const userLoginValidator = () => {
+    return [
+        body('email').optional().isEmail().withMessage("Email is invalid ❌"),
+        body('password').notEmpty().withMessage("Password is required...")
+    ]
+}
+
+const userChangeCurrentPasswordValidator = () => {
+    return [
+        body('oldPassword').notEmpty().withMessage("Old password is required"),
+        body('newPassword').notEmpty().withMessage("New password is required"),
+    ]
+}
+
+const userForgotPasswordValidator = () => {
+    return [
+        body("email").notEmpty().withMessage("Email is required").isEmail().withMessage("Email is invalid")
+    ]
+}
+
+const userResetForgotPasswordValidator = () => {
+    return[
+        body("newPassword").notEmpty().withMessage("Password is required")
+    ]
+}
+
 export {
-    userRegisterValidator
+    userRegisterValidator,
+    userLoginValidator,
+    userChangeCurrentPasswordValidator,
+    userForgotPasswordValidator
 }
